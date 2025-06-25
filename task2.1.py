338e36ae48d6a2bb289bfb9a007ff20ab4c3abb7
muskanmaksud06 <mallebharimuskan30@gmail.com>
2025-06-25 16:04:55 +0530

    Add files via upload

---
+++ task2.1.py
@@ -0,0 +1,6 @@
+#To check whether a number is EVEN or ODD using if-else statements:
+n=int(input("Enter a number:"))
+if n%2==0:
+    print("Even")
+else:
+    print("Odd")
---
+++ task2.2.py
@@ -0,0 +1,6 @@
+#To find a sum of number from 1-50 using a for loop:
+n=list(range(1,51))
+sum=0
+for i in n:
+   sum+=i
+print("The sum of numbers from 1 to 50 is: ",sum)
